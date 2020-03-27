f475c583fe90f1598d0201e7ea78f65af6cd192d
knqyf263 <knqyf263@gmail.com>
2020-03-27 12:46:51 +0300

    release test

--- ci/deploy-deb.sh
+++ ci/deploy-deb.sh
@@ -5,10 +5,14 @@ UBUNTU_RELEASES=$(ubuntu-distro-info --supported)
 
 cd trivy-repo/deb
 
-for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
-  echo "Adding deb package to $release"
+for release in $(reprepro ls trivy | awk -F "|" '{print $3}' | sed 's/ //g'); do
+  echo "Removing deb package of $release"
   reprepro -A i386 remove $release trivy
   reprepro -A amd64 remove $release trivy
+done
+
+for release in ${DEBIAN_RELEASES[@]} ${UBUNTU_RELEASES[@]}; do
+  echo "Adding deb package to $release"
   reprepro includedeb $release ../../dist/*Linux-64bit.deb
   reprepro includedeb $release ../../dist/*Linux-32bit.deb
 done
